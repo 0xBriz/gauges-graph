1e32cfdd215415e2f16eb8348bb69cca871de010
0xBriz <chaindevone@gmail.com>
2022-08-06 08:11:49 -0400

    clean up for our use case

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,6 +10,7 @@ export class AddressByNetwork {
   public mainnet: string;
   public kovan: string;
   public goerli: string;
+  public bsc: string;
 }
 
 let network: string = dataSource.network();
@@ -17,7 +18,8 @@ let network: string = dataSource.network();
 let controllerAddressByNetwork: AddressByNetwork = {
   mainnet: '0xC128468b7Ce63eA702C1f104D55A2566b13D3ABD',
   kovan: '0x28bE1a58A534B281c3A22df28d3720323bfF331D',
-  goerli: '0xBB1CE49b16d55A1f2c6e88102f32144C7334B116'
+  goerli: '',
+  bsc: ''
 };
 
 function forNetwork(
@@ -28,8 +30,10 @@ function forNetwork(
     return Address.fromString(addressByNetwork.mainnet);
   } else if (network == 'kovan') {
     return Address.fromString(addressByNetwork.kovan);
-  } else {
+  } else if(network == 'goerli') {
     return Address.fromString(addressByNetwork.goerli);
+  } else {
+    return Address.fromString(addressByNetwork.bsc);
   }
 }
 
